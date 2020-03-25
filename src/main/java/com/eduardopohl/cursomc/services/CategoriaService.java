c29ad94dbe7ac0979e14a9f02e62375212e16752
Eduardo Pohl <eduardo.pohl@gmail.com>
2020-03-24 21:59:01 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/eduardopohl/cursomc/services/CategoriaService.java
+++ src/main/java/com/eduardopohl/cursomc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.eduardopohl.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardopohl.cursomc.domain.Categoria;
 import com.eduardopohl.cursomc.repositories.CategoriaRepository;
+import com.eduardopohl.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +18,8 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
