1299e30b276089d159970ad26ccfb3d764e2caba
Eduardo Pohl <eduardo.pohl@gmail.com>
2020-03-28 19:16:55 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/eduardopohl/cursomc/services/CategoriaService.java
+++ src/main/java/com/eduardopohl/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.eduardopohl.cursomc.domain.Categoria;
+import com.eduardopohl.cursomc.dto.CategoriaDTO;
 import com.eduardopohl.cursomc.repositories.CategoriaRepository;
 import com.eduardopohl.cursomc.services.exceptions.DataIntegrityException;
 import com.eduardopohl.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
