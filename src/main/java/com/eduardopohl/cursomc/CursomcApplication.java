6909df15a703d8b2128a6a049437314ceedc97da
Eduardo Pohl <eduardo.pohl@gmail.com>
2020-03-31 11:39:18 -0300

    Email HTML

--- src/main/java/com/eduardopohl/cursomc/CursomcApplication.java
+++ src/main/java/com/eduardopohl/cursomc/CursomcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -14,5 +14,5 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	}
-	
+
 }
