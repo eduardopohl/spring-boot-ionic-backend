59d67d44b11b8c8a600941ae19081288ab1f7898
Eduardo Pohl <eduardo.pohl@gmail.com>
2020-04-18 11:34:16 -0300

    Modo de producao

---
+++ src/main/java/com/eduardopohl/cursomc/config/ProdConfig.java
@@ -0,0 +1,41 @@
+package com.eduardopohl.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.eduardopohl.cursomc.services.DBService;
+import com.eduardopohl.cursomc.services.EmailService;
+import com.eduardopohl.cursomc.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+
+}
